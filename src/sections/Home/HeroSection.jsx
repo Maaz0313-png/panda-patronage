9cdeb72e675d8a157258ad96b1d275dbf4648cc9
Maaz0313-png <maaztajammul63@gmail.com>
2025-07-11 00:05:01 +0500

    feat: enhance layout and responsiveness in HeroSection component

--- src/sections/Home/HeroSection.jsx
+++ src/sections/Home/HeroSection.jsx
@@ -24,11 +24,11 @@ const HeroSection = () => {
     <ScrollReveal>
       <section className="tablet:p-6 tablet:pb-20 desktop:pb-24 flex min-h-[600px] items-center justify-center bg-transparent p-4 pb-12">
         <div
-          className="grid-cols-12 tablet:px-10 tablet:py-20 desktop:p-16 desktop:pt-[100px] relative grid w-full max-w-[1195px] items-center rounded-xl [background-image:linear-gradient(116deg,_rgb(152,59,0)_0%,_rgb(92,36,0)_9%,_rgb(31,31,31)_31%)] px-4 py-14 shadow-lg"
+          className="tablet:px-10 tablet:py-20 desktop:p-16 desktop:pt-[100px] desktop:grid-cols-2 tablet:grid-cols-12 relative grid w-full max-w-[1195px] items-center rounded-xl [background-image:linear-gradient(116deg,_rgb(152,59,0)_0%,_rgb(92,36,0)_9%,_rgb(31,31,31)_31%)] px-4 py-14 shadow-lg"
           style={{ minHeight: 480 }}
         >
           {/* Left: Text */}
-          <div className="tablet:col-span-8 col-span-9 z-10 flex w-full flex-col justify-center gap-4">
+          <div className="tablet:col-span-8 desktop:col-span-1 z-10 flex w-full flex-col justify-center gap-4">
             <h1 className="tablet:text-[75px] tablet:w-full h-min w-fit text-left text-5xl leading-[90px] font-thin tracking-[-3.75px] text-white">
               We&apos;re Creative <br className="tablet:hidden inline" />{" "}
               Digital Marketing Agency!
@@ -38,7 +38,7 @@ const HeroSection = () => {
               paving the way for your success without limits.
             </p>
             <Link
-              className={`tablet:mt-6v tablet:w-fit relative flex size-fit w-full cursor-pointer items-center overflow-hidden rounded-xl bg-[rgba(255,255,255,0.2)] px-0 py-1.5 text-white`}
+              className={`tablet:mt-6 tablet:w-fit relative flex size-fit w-full cursor-pointer items-center overflow-hidden rounded-xl bg-[rgba(255,255,255,0.2)] px-0 py-1.5 text-white`}
               to="/contact"
               onMouseEnter={() => setCtaHovered(true)}
               onMouseLeave={() => setCtaHovered(false)}
@@ -69,12 +69,12 @@ const HeroSection = () => {
             </Link>
           </div>
           {/* Right: Panda and shapes */}
-          <div className="tablet:mt-0 desktop:w-[601px] relative mt-10 flex tablet:col-span-4 col-span-4 min-h-[370px] items-center justify-center">
+          <div className="tablet:mt-0 desktop:w-[601px] tablet:col-span-4 desktop:col-span-1 relative mt-10 flex min-h-[370px] items-center justify-center">
             {/* Panda image */}
             <motion.img
               src="/images/misc/panda-hero.png"
               alt="Panda with headphones"
-              className="desktop:opacity-100 desktop:block desktop:h-[515px] desktop:w-[601px] absolute right-1 bottom-0 z-10 hidden cursor-grab rounded-br-3xl rounded-bl-3xl opacity-0"
+              className="desktop:opacity-100 desktop:block desktop:h-[515px] desktop:w-[601px] absolute right-0 -bottom-24 z-10 hidden cursor-grab rounded-br-3xl rounded-bl-3xl opacity-0"
               drag={true}
               dragElastic={1}
               dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
@@ -92,7 +92,7 @@ const HeroSection = () => {
             <motion.img
               src="/images/misc/hero-left-shape.png"
               alt="Left shape"
-              className="tablet:hidden desktop:block tablet:-bottom-40 tablet:left-[-60px] tablet:h-[209.625px] tablet:w-[217px] absolute top-[57%] right-[-5%] z-10 w-[149px] cursor-grab"
+              className="tablet:hidden desktop:block tablet:-bottom-40 tablet:left-[-60px] tablet:h-[209.625px] tablet:w-[217px] desktop:top-9/10 desktop:-left-9 absolute top-[57%] z-10 w-[149px] cursor-grab"
               drag={true}
               dragElastic={1}
               dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
@@ -117,7 +117,7 @@ const HeroSection = () => {
             <motion.img
               src="/images/misc/hero-right-shape.png"
               alt="Right shape"
-              className="desktop:right-[-10px] tablet:block desktop:bottom-[-10px] tablet:right-[10px] tablet:bottom-[-90px] tablet:size-[180px] desktop:size-[130px] absolute right-[0px] bottom-[-10px] z-10 hidden size-44 cursor-grab"
+              className="desktop:-right-2 tablet:block desktop:bottom-8 tablet:right-[10px] tablet:bottom-[-90px] tablet:size-[180px] desktop:size-[130px] absolute right-[0px] bottom-[-10px] z-10 hidden size-44 cursor-grab"
               drag={true}
               dragElastic={1}
               dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
